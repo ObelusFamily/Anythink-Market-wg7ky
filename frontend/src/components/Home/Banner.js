05a9c058d96976001a5aac7dea7af2993e103485
irshit mukherjee <mukehrjeeirshit50@gmail.com>
2022-08-07 09:43:52 +0530

    added search filter in frontend by using title field

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,13 +1,35 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import { useState } from "react";
+import agent from "../../agent";
 
-const Banner = () => {
+const Banner = (props) => {
+  const [searchInput, setSearchInput] = useState("");
+
+  const searchbyTitle = (searchvalue) => {
+    setSearchInput(searchvalue);
+
+    if (searchInput.length >= 3) {
+      console.log("hey this is" + searchvalue);
+      props.onClickTitle(
+        searchInput,
+        (page) => agent.Items.byTitle(searchInput, page),
+        agent.Items.byTitle(searchInput)
+      );
+    }
+  };
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span id="get-part">A place to get</span>
+          <input
+            id="search-box"
+            className="rounded mx-4 px-4 py-2 w-50"
+            placeholder="What is it that you truly desire?"
+            onChange={(e) => searchbyTitle(e.target.value)}
+          />
           <span> the cool stuff.</span>
         </div>
       </div>
