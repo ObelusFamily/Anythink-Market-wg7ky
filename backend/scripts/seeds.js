3b854593a70391012ce0d15a213a6d8dcc0a0848
irshit mukherjee <mukehrjeeirshit50@gmail.com>
2022-08-09 03:59:35 +0530

    data seeding added

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,1 +1,52 @@
-//TODO: seeds script should come here, so we'll be able to put some data in our local env
+//TODO: seeds script should come here, so we'll be able to put some data in our local
+var mongoose = require("mongoose");
+require("dotenv").config();
+require("../models/User");
+require("../models/Item");
+require("../models/Comment");
+
+var Item = mongoose.model("Item");
+var Comment = mongoose.model("Comment");
+var User = mongoose.model("User");
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log())
+  .catch((err) => console.log(err));
+
+const SeedData = async () => {
+  try {
+    await User.deleteMany();
+    await Item.deleteMany();
+    await Comment.deleteMany();
+
+    for (let i = 1; i <= 100; i++) {
+      const user = new User();
+      user.username = `username${i}`;
+      user.email = `email${i}@g.com`;
+      user.setPassword(`password${i}`);
+      await user.save();
+     
+      const item= new Item();
+      item.title=`title${i}`;
+      item.description=`desc${i}`
+      await item.save();
+
+      const comment=new Comment();
+      comment.body=`comment${i}`
+      await comment.save();
+
+    }
+
+    console.log("success");
+    
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+SeedData();
